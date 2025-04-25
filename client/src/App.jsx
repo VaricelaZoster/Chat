9dd68db0196256aa2eb84baa91e8dab7f1684fd3
VaricelaZoster <maildwaipayansaha@gmail.com>
2025-04-25 17:17:57 +0530

    Ceating login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import {BrowserRouter as Router,Routes,Route, BrowserRouter} from 'react-router-
 
 const Home = lazy(() => import("./pages/Home"))
 const Login = lazy(() => import("./pages/login"))
+const Chat = lazy(() => import('./pages/Chat'))
+const Group = lazy(() => import('./pages/Group'))
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element = {<Home/>}/>
         <Route path="/login" element = {<Login/>}/>
+        <Route path="/chat/:chatId" element = {<Chat/>}/>
+        <Route path="/group" element = {<Group/>}/>
       </Routes>
     </BrowserRouter>
   )
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Button, Container, Paper, TextField, Typography} from '@mui/material'
+
+
 
 const login = () => {
+  const [isLogin,setIsLogin] = useState(true)
+  const toggleLogin = () => setIsLogin(false)
   return (
-    <div>login</div>
+    <Container component={"main"} maxWidth="xs"
+      sx={{height:"100vh",
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+      }}>
+      <Paper elevation={3} sx = {{padding: 4, display: "flex", flexDirection: "column",alignItems: "center"}}>
+        {isLogin ? <>
+          <Typography variant='h5'>Login</Typography>
+          <form style = {{
+            width:"100%",
+            marginTop:"1rem",
+          }}>
+            <TextField required fullWidth label = "Username" margin="normal" variant="outlined"/>
+            <TextField required fullWidth label = "Password" type="password" margin="normal" variant="outlined"/>
+            <Button sx = {{marginTop:"1rem"}} variant="contained" color="primary" type="submit" fullWidth>Login</Button>
+            <Typography textAlign={"center"} m={"1rem"}>Or</Typography>
+            <Button variant="text" onClick={toggleLogin} fullWidth>Sign Up Instead</Button>
+          </form>
+        </>:<span>Register</span>}
+      </Paper>
+    </Container>
   )
 }
 
