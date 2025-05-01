be2718612f06e33bd71b607c67ddd4ad1743c2c5
VaricelaZoster <maildwaipayansaha@gmail.com>
2025-05-01 17:47:11 +0530

    Fixed grid issue

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,8 +1,27 @@
+import { Height } from '@mui/icons-material'
+import { AppBar, Box, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { orange } from '../../constants/color'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <Box sx = {{flexGrow:1}} height = {"4rem"}>
+      <AppBar position="static" sx = {{
+        bgcolor:orange
+      }}>
+        <Toolbar>
+          <Typography 
+          variant='h6'
+          sx={{
+            display:{xs:"none",sm:"block"},
+          }}>
+            Chat App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </>
   )
 }
 
